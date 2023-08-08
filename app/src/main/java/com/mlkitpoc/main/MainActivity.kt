dd9395368ada17b10aa955417903a177525afbc0
Sagar Vora <sagar.vora@dmartindia.com>
2023-08-08 12:28:25 +0530

    adding Anushree's code

---
+++ app/src/main/java/com/mlkitpoc/main/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.mlkitpoc.main
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mlkitpoc.R
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    var list = ArrayList<String>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // setting up recycler view
+        val groceryList = findViewById<RecyclerView>(R.id.recycler_main_list)
+        groceryList.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        groceryList.adapter = MainAdapter(list)
+
+        // add button which takes user input
+        val addButton = findViewById<Button>(R.id.add_item_button)
+        addButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            builder.setTitle("Add Item")
+            builder.setNeutralButton("Cancel") { dialog, i -> }
+            val dialogLayout = inflater.inflate(R.layout.add_item_popup, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.editText)
+            builder.setView(dialogLayout)
+            builder.setPositiveButton("OK") { dialog, i ->
+                list.add(editText.text.toString())
+                groceryList.adapter = MainAdapter(list)
+            }
+            builder.show()
+        }
+
+        // goes to scan activity
+        val scanButton = findViewById<Button>(R.id.scan_button)
+        scanButton.setOnClickListener {
+//            val launchScan = Intent(this, ScanActivity::class.java)
+//            startActivity(launchScan)
+        }
+
+        // goes to results (list activity)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        doneButton.setOnClickListener {
+//            val launchList = Intent(this, ListActivity::class.java)
+//            launchList.putStringArrayListExtra("itemList", list)
+//            startActivity(launchList)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/mlkitpoc/main/MainAdapter.kt
@@ -0,0 +1,66 @@
+package com.mlkitpoc.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+import com.mlkitpoc.R
+
+
+class MainAdapter(val userList: ArrayList<String>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    lateinit var parentholder: ViewGroup
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val itemName = itemView.findViewById<CheckBox>(R.id.checkbox)
+        val deleteButton = itemView.findViewById<ImageButton>(R.id.delete_item)
+        val editButton = itemView.findViewById<ImageButton>(R.id.edit_item_button)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.checklist_item, parent, false)
+        parentholder = parent
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // displays item
+        holder?.itemName?.setText(userList[position])
+
+        // deletes item
+        holder?.deleteButton?.setOnClickListener {
+            userList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, userList.size)
+            holder.itemView.setVisibility(View.GONE)
+        }
+
+        // edits item
+       holder?.editButton?.setOnClickListener {
+            val builder = AlertDialog.Builder(it.context)
+            val inflater = LayoutInflater.from(parentholder?.context)
+            builder.setTitle("Edit Item")
+            builder.setNeutralButton("Cancel") { dialog, i -> }
+            val dialogLayout = inflater.inflate(R.layout.add_item_popup, null)
+            val editText  = dialogLayout.findViewById<EditText>(R.id.editText)
+            editText.setText(userList[position])
+            builder.setView(dialogLayout)
+            builder.setPositiveButton("OK") { dialog, i ->
+                userList.removeAt(position)
+                userList.add(position, editText.text.toString())
+                notifyDataSetChanged()
+                notifyItemChanged(position)
+            }
+            builder.show()
+        }
+
+    }
+}
