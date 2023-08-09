63cc3f09a7bdd10c06237ed520df42e4a179d2fe
anidasu <anidasu14@gmail.com>
2023-08-09 11:33:54 +0530

    Updated for permission

--- app/src/main/java/com/mlkitpoc/image/ImageUploadActivity.kt
+++ app/src/main/java/com/mlkitpoc/image/ImageUploadActivity.kt
@@ -1,7 +1,9 @@
 package com.mlkitpoc.image
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
@@ -9,23 +11,27 @@ import android.provider.MediaStore
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.mlkitpoc.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.mlkitpoc.R;
 
 class ImageUploadActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_IMAGE_PICK = 1
         private const val REQUEST_CAMERA_CAPTURE = 2
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 3
+        private var denied = false
     }
 
-    private val takePicture =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                val imageBitmap = data?.extras?.get("data") as Bitmap?
-                startImageDisplayActivity(imageBitmap, null)
-            }
+    private val takePicture = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val imageBitmap = data?.extras?.get("data") as Bitmap?
+            startImageDisplayActivity(imageBitmap, null)
         }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,24 +42,26 @@ class ImageUploadActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.cameraButton).setOnClickListener {
-            openCameraForListScan()
+            if (hasCameraPermission()) {
+                openCameraForListScan()
+            }
+            else {
+                requestCameraPermission()
+            }
         }
     }
 
-    private val getContent =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri: Uri? ->
-            if (imageUri != null) {
-                startImageDisplayActivity(null, imageUri)
-            }
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri: Uri? ->
+        if (imageUri != null) {
+            startImageDisplayActivity(null, imageUri)
         }
-
+    }
 
     private fun openGallery() {
         getContent.launch("image/*")
     }
 
     private fun openCameraForListScan() {
-        //permission
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         takePicture.launch(intent)
     }
@@ -64,5 +72,45 @@ class ImageUploadActivity : AppCompatActivity() {
             putExtra(ImageDisplayActivity.EXTRA_IMAGE_URI, imageUri)
         }
         startActivity(intent)
+    }
+
+    private fun hasCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+
+            val snackbar = Snackbar.make(
+                findViewById(R.id.rootLayout),
+                "Camera permission is required to take pictures",
+                Snackbar.LENGTH_INDEFINITE
+            )
+            snackbar.setAction("Grant") {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_REQUEST_CODE
+                )
+            }
+            snackbar.show()
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+
+    // Handle permission request result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCameraForListScan()
+            } else {
+                denied = true
+            }
+        }
     }
 }
