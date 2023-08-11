44c2cedee021587ea6b6ba507e1fed3d7a0dddda
anushm <mishra24@illinois.edu>
2023-08-11 10:55:01 +0530

    fixed checklist bug in list fragment

--- app/src/main/java/com/mlkitpoc/integration/ListFragment.kt
+++ app/src/main/java/com/mlkitpoc/integration/ListFragment.kt
@@ -1,18 +1,14 @@
 package com.mlkitpoc.integration
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.view.indices
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.mlkitpoc.R
 import com.mlkitpoc.databinding.FragmentListBinding
+import com.mlkitpoc.integration.ml.model.Items
 import com.mlkitpoc.integration.viewmodel.ContentViewModel
 import com.mlkitpoc.list.ListAdapter
 
@@ -21,7 +17,7 @@ class ListFragment : Fragment() {
     private var binding: FragmentListBinding? = null
     private val viewModel: ContentViewModel by activityViewModels()
     private lateinit var itemAdapter: ListAdapter
-    private var itemsList = mutableListOf<String>()
+    private var itemsList = mutableListOf<Items>()
 
 
     var i:Int = 0
@@ -32,19 +28,19 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        itemAdapter = ListAdapter(itemsList, onItemClick = {
-            viewModel.setText(it)
-            itemAdapter.notifyDataSetChanged()
-        })
-        binding?.rvList?.apply {
-            layoutManager = LinearLayoutManager(this.context)
-            adapter = itemAdapter
-
-        }
         viewModel.itemList.observe(viewLifecycleOwner) {
-            itemsList.addAll(it)
+            it.forEach { text ->
+                itemsList.add(Items(text, false))
+            }
+
+            itemAdapter = ListAdapter(itemsList) { text ->
+                viewModel.setText(text)
+            }
+            binding?.rvList?.apply {
+                layoutManager = LinearLayoutManager(this.context)
+                adapter = itemAdapter
+            }
             viewModel.setText(it[i])
-            itemAdapter.notifyDataSetChanged()
         }
     }
 
--- app/src/main/java/com/mlkitpoc/list/ListAdapter.kt
+++ app/src/main/java/com/mlkitpoc/list/ListAdapter.kt
@@ -5,18 +5,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.mlkitpoc.R
 import com.mlkitpoc.integration.DetailsFragment
+import com.mlkitpoc.integration.ml.model.Items
 
-class ListAdapter(private val itemList: List<String>,
+class ListAdapter(private val itemList: List<Items>,
 private val onItemClick: (String)-> Unit) :
     RecyclerView.Adapter<ListAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val itemNameTextView: TextView = itemView.findViewById(R.id.itemname)
+        val checkBox: CheckBox = itemView.findViewById(R.id.checkBox)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -27,9 +30,17 @@ private val onItemClick: (String)-> Unit) :
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = itemList[position]
-        holder.itemNameTextView.text = item
+        holder.itemNameTextView.text = item.text
+        holder.checkBox.isChecked = item.isChecked
+        Log.d("TAG", "onBindViewHolder: pos before " + position)
+        Log.d("TAG", "onBindViewHolder: is checked " + item.isChecked)
         holder.itemNameTextView.setOnClickListener {
-            onItemClick(item)
+            onItemClick(item.text)
+        }
+
+        holder.checkBox.setOnCheckedChangeListener { compoundButton, b ->
+            Log.d("TAG", "onBindViewHolder: pos on click" + position)
+            itemList[position].isChecked = b
         }
 
     }
